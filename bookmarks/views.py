2290ad72fb775e5fd1af800a8d80fba18bdce17c
Patrick Cash <PatrickCash@gmail.com>
2019-02-20 14:15:08 -0500

    added support to filter on tag

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
+from django.urls import reverse
 
 from bookmarks.forms import BookmarkModelForm
 from bookmarks.models import Bookmark
@@ -11,11 +12,20 @@ class BookmarkList(generic.ListView):
     queryset = Bookmark.objects.all()
 
 
+class BookmarkListByTag(generic.ListView):
+    template_name = 'bookmarks/bookmark_list.html'
+
+    def get_queryset(self):
+        tag = self.request.GET.get('tag')
+        queryset = Bookmark.objects.filter(tags__name__in=[tag])
+        return queryset
+
+
 def create_bookmark(request):
     form = BookmarkModelForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('/')
+        return redirect(reverse('bookmarks'))
     context = {
         'form': form
     }
@@ -27,7 +37,7 @@ def edit_bookmark(request, slug):
     form = BookmarkModelForm(request.POST or None, instance=bookmark)
     if form.is_valid():
         form.save()
-        return redirect('/')
+        return redirect(reverse('bookmarks'))
     context = {
         'form': form
     }
@@ -37,4 +47,4 @@ def edit_bookmark(request, slug):
 def delete_bookmark(request, slug):
     bookmark = get_object_or_404(Bookmark, slug=slug)
     bookmark.delete()
-    return redirect('/')
+    return redirect(reverse('bookmarks'))
--- delicious_django/urls.py
+++ delicious_django/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from bookmarks.views import BookmarkList, create_bookmark, edit_bookmark, delete_bookmark
+from bookmarks.views import BookmarkList, BookmarkListByTag, create_bookmark, edit_bookmark, delete_bookmark
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BookmarkList.as_view(), name='bookmarks'),
+    path('tags/', BookmarkListByTag.as_view(), name='bookmarks_by_tag'),
     path('create/', create_bookmark),
     path('edit/<slug:slug>/', edit_bookmark),
     path('delete/<slug:slug>/', delete_bookmark),
